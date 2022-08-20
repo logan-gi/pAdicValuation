0811557bf131b7ea4e840bd6fb706aaadec4a62d
Logan Giralt <111460026+logan-gi@users.noreply.github.com>
2022-08-19 17:46:35 -0700

    Add files via upload
    Finds p-adic valuation and absolute value!

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// This program will take the p-adic valuation of some rational number n in prime base p
+// Program finds the p-adic valuation and absolute value of a rational number n based on its prime base
 
 #include <iostream>
 #include <cstdlib>
@@ -8,14 +8,13 @@ using namespace std;
 const long TENBIL = 100000;
 int exponent(0);
 int base;
-long num, denom;
+long long num, denom;
 char ans, answer;
-
 
 long long gcf(long long number);
 // Searches for greatest common factor among numerator and denominator
 
-long long gcfCheck(long numb1, long denum);
+long long gcfCheck(long long numb1, long long denum);
 // Redundant greatest common factor check
 
 bool ifPrime(int numb2);
@@ -31,31 +30,38 @@ int main()
 {
     double long rational;
     do {
-        cout << "What prime number set would you like to use?" << endl;
+        cout << "What prime would you like to use?" << endl;
         cin >> base;
         if (ifPrime(base)) {
-            cout << "Are you entering a fraction or decimal?\n";
-            cin >> ans;
-            if (ans == 'F' || ans == 'f') {
-                cout << "Enter your numerator:" << endl;
-                cin >> num;
-                cout << "Enter your denominator:" << endl;
-                cin >> denom;
-                long hold = num;
-                num = num/gcfCheck(hold, denom);
-                denom = denom/gcfCheck(hold, denom);
-            }
-            if (ans == 'D' || ans == 'd') {
-                cout << "Enter a rational number (up to 5 decimal points):\n";
-                cin >> rational;
-                fractionFinder(rational);
-            }
-            cout << "Your p-adic valuation for the rational number \"" << static_cast<double>(num)/denom << "\" aka \""
-                << num << "/" << denom
-                 << "\" in the " << base << "-adic set is: "
-                 << pValuation(exponent, base, num)
-                 - pValuation(exponent, base, denom) << endl;
-        } else
+        cout << "Are you entering a fraction or decimal?\n";
+        cin >> ans;
+        if (ans == 'F' || ans == 'f') {
+            cout << "Enter your numerator:" << endl;
+            cin >> num;
+            num = abs(num);
+            cout << "Enter your denominator:" << endl;
+            cin >> denom;
+            denom = abs(denom);
+            long hold = num;
+            num = num / gcfCheck(hold, denom);
+            denom = denom / gcfCheck(hold, denom);
+        }
+        if (ans == 'D' || ans == 'd') {
+            cout << "Enter a rational number (up to 5 decimal points):\n";
+            cin >> rational;
+            rational = abs(rational);
+            fractionFinder(rational);
+        }
+        cout << "Your p-adic valuation for the rational number \""
+             << static_cast<double>(num) / denom << "\" aka \""
+             << num << "/" << denom
+             << "\" in the " << base << "-adic set is: "
+             << pValuation(exponent, base, num)
+                - pValuation(exponent, base, denom) << endl;
+        cout << "The absolute value of \"" << static_cast<double>(num) / denom << "\" is "
+             << (1.0)/(pow(base, (pValuation(exponent, base, num)
+                                    - pValuation(exponent, base, denom)))) << endl;
+    } else
             cout << "Invalid input\n";
         cout << "Would you like to find another?\n";
         cout << "Enter 'Y' or 'N'\n";
@@ -65,17 +71,15 @@ int main()
     return 0;
 }
 
+
 bool ifPrime(int numb2)
 {
     if (numb2 == 0 || numb2 == 1)
         return false;
     for (int i=2; i <= numb2/2; ++i)
-    {
-        if (numb2 % i == 0) {
+        if (numb2 % i == 0)
             return false;
-        }
-    }
-        return true;
+    return true;
 }
 
 int pValuation (int expo, int base1, long ration)
@@ -96,7 +100,7 @@ int pValuation (int expo, int base1, long ration)
 
 void fractionFinder(double long numb)
 {
-    long double frac = (numb)*TENBIL;
+    long long frac = (numb)*TENBIL;
     num = (frac)/gcf(frac);
     denom = (TENBIL)/gcf(frac);
     long check = num;
@@ -113,8 +117,9 @@ long long gcf(long long number)
             return i;
 }
 
-long long gcfCheck(long numb1, long denum) {
+long long gcfCheck(long long numb1, long long denum) {
     for (long i = numb1; i > 0; i--)
         if (numb1 % i == 0 && denum % i == 0)
             return i;
+    return 1;
 }
